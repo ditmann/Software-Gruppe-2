f6c04012453736ca4c7215850e2a2d74f2206b81
Kristian Tjernshaugen Husby <35239676+Hillpond@users.noreply.github.com>
2025-10-05 23:59:48 +0200

    Added enturclient
    Made interface for enturAPI and wrote comments on what everything does for easy use, also made it write everything into a json-file

--- avandra/src/main/java/no/avandra/Main.java
+++ avandra/src/main/java/no/avandra/Main.java
@@ -1,4 +1,22 @@
 package no.avandra;
 
-import no.avandra.classes.Destinasjon;
 
+
+import no.avandra.classes.EnturHttpClient;
+import no.avandra.entur.EnturClient;
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String clientName = System.getenv().getOrDefault("ET_CLIENT_NAME", "HIOFsTUD-AVANDRA");
+        EnturClient entur = new EnturHttpClient(clientName);
+
+        entur.planTripCoordsToFile(
+                59.9139, 10.7522,   // from (Oslo approx)
+                60.1976, 11.1004,   // to (OSL area approx)
+                3,
+                true                // include request metadata in file
+        );
+    }
+}
