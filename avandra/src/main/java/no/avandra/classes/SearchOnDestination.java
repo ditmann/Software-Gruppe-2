0054dbe1283ad25afc6b1b1d17cd00fdc0747fbf
Tinakringen <tinakringen98@gmail.com>
2025-10-08 12:20:28 +0200

    :memo:
    Endret på JSON filen slik at den nå har liste i liste.

--- avandra/src/main/java/no/avandra/classes/SearchOnDestination.java
+++ avandra/src/main/java/no/avandra/classes/SearchOnDestination.java
@@ -1,26 +1,29 @@
 package no.avandra.classes;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SearchOnDestination implements SearchOnDestinationPort {
 
-    private File getJSON;
-    private ObjectMapper objectMapper = new ObjectMapper();
-    File JSONfile = new File(/no.avandra.classes.)
-
     public SearchOnDestination(File getJSON) {
-        this.getJSON = getJSON;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            List<Coordinate> kordinater = objectMapper.readValue(getJSON, new TypeReference<List<Coordinate>>() {});
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Coordinate destinationCoordinate(){
-
-
 
 
 
