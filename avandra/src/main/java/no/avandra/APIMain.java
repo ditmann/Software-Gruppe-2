076e7726a71148f64d7175a9738cc6e66159a236
SteinOsen91 <stein.ose91@gmail.com>
2025-10-20 09:13:09 +0200

    Added Reise instance variables and TripPart to store information from the API call.

--- avandra/src/main/java/no/avandra/APIMain.java
+++ avandra/src/main/java/no/avandra/APIMain.java
@@ -8,14 +8,14 @@ import no.avandra.classes.*;
 public class APIMain {
     public static void main(String[] args) throws Exception {
         String clientName = "HIOFsTUD-AVANDRA";
-        LocationPort location = new RandomLocationAdapter();
+        LocationPort location = new IpGeolocationAdapter(clientName);
         Coordinate me = location.currentCoordinate();
         EnturClient entur = new EnturHttpClient(clientName);
 
         entur.planTripCoordsToFile(
                 me.getLatitudeNum(), me.getLongitudeNUM(),   // from you
-                59.136205, 11.382195,   // to moss// )
-                3, // amount of different trips
+                59.553999, 11.334520,   // to mysen// )
+                1, // amount of different trips
                 true                // include request metadata in file
         );
     }
