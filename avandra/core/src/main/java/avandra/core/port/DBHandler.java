bf89857cf15e1515ced5fc9c1cf1812ece51a0b2
Adrian Ditmansen <adriandenutrolige@gmail.com>
2025-10-24 10:29:28 +0200

    prøver igjen fortsatt trøbbel fra min side

--- avandra/core/src/main/java/avandra/core/port/DBHandler.java
+++ avandra/core/src/main/java/avandra/core/port/DBHandler.java
@@ -1,10 +1,11 @@
 package avandra.core.port;
 
-public interface DBHandler {
 
 import avandra.core.domain.Coordinate;
-    // make user in DB
-    public void createUser(String key, Object object);
+
+public interface DBHandler {
+    // make user in DB %overloading skal brukes%
+    public void createUser( String userID, boolean adminUser);
 
     /// @param addValue is Object to allow appending Documents, ArrayLists and Strings
     public void appendData(String id, String addKey, Object addValue);
@@ -12,11 +13,17 @@ import avandra.core.domain.Coordinate;
     /// returns arraylist in mongo, object in json. COULD both be arraylist though pointless for json
     public Object retrieveAllData();
 
+    /// Legger til destinasjon til favoritter
+    public void addDestinationToFavorites(String userID, String destinationName, String address, double latitude, double longitude);
+
+    /// Legger til koordinater i lagret destinasjon
+    public void addCoordinatesToDestination(String userID, String destinationName, double latitude, double longitude);
+
     ///  Returnerer koordinater
     public Coordinate destinationCoordinate(String name);
 
-    //search with ID to find specific document
-    public Object searchDestination(String userID, String destinationType, String destinationID);
+    /// search with ID to find specific document
+    public Coordinate searchDestination(String userID, String destinationID);
 
     //Removes data (dokument id, what to delete, path, path, path) %overloading skal brukes%
     public void removeData(String userID); //sletter topp nivå i dokument
