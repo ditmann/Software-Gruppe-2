eb3df3d90218fa88766fe6bb96b347b11f0fbc2a
beony <siljestrkrsn@gmail.com>
2025-10-26 17:12:32 +0100

    💩 i tried
    Creating MongoDBConnectionTest

--- avandra/app/src/main/java/avandra/app/Main.java
+++ avandra/app/src/main/java/avandra/app/Main.java
@@ -3,10 +3,12 @@ package avandra.app;
 
 import avandra.storage.adapter.MongoDBConnection;
 import avandra.storage.adapter.MongoDBHandler;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
+/*
 
         MongoDBConnection connection = new MongoDBConnection();
         MongoDBHandler handler = new MongoDBHandler(connection);
@@ -14,7 +16,11 @@ public class Main {
 
         MongoDBConnection connection1 = new MongoDBConnection();
         MongoDBHandler handler1 = new MongoDBHandler(connection1);
-        handler1.appendData("roflmao", "age", 12);
+        handler1.appendData("roflmao", "age", 12);*/
+
+        MongoDBConnection conn = new MongoDBConnection();
+        MongoCollection<Document> colldoc = conn.getCollection();
+        System.out.println(colldoc);
 
 
     }
---
+++ avandra/test/src/main/java/avandra/test/MongoDBConnectionTest.java
@@ -0,0 +1,35 @@
+package avandra.test;
+
+import avandra.storage.adapter.MongoDBConnection;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.junit.jupiter.api.*;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MongoDBConnectionTest {
+
+    @Test
+    void open_opensDatabase() throws Exception {
+        /// Tests the purpose of open(): to create a connection to the actual database.
+        /// Proven by returning /something/ and not nothing
+        MongoDBConnection connection = new MongoDBConnection(); //add params if applicable
+        try (MongoDBConnection newConnection = connection.open()) {
+            MongoCollection<Document> collection = newConnection.getCollection();
+            Assertions.assertNotNull(collection);
+        }
+    }
+
+    @Test
+    /// Testing that open() returns an instance of the connection-class that is equal to the original
+    void open_returnsThis() throws Exception{
+        MongoDBConnection fakeConnection = mock(MongoDBConnection.class);
+
+        when(fakeConnection.open()).thenReturn(fakeConnection);
+        MongoDBConnection newConnection = fakeConnection.open();
+        Assertions.assertSame(fakeConnection, newConnection);
+
+    }
+}
+
