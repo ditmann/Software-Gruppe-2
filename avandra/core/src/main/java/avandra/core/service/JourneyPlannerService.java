d6ada43985140cc8d811d6ef2b4c97361ee89604
Adrian Ditmansen <adriandenutrolige@gmail.com>
2025-11-06 09:49:28 +0100

    :sparkels: JourneyPlannerService done!

---
+++ avandra/core/src/main/java/avandra/core/service/JourneyPlannerService.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import avandra.core.DTO.CoordinateDTO;
+import avandra.core.port.LocationPort;
+import avandra.core.service.DBService;
+
+
+public class JourneyPlannerService {
+
+    LocationPort locationPort;
+    DBService dbService;
+
+    public JourneyPlannerService(LocationPort locationPort, DBService dbService) {
+        this.locationPort = locationPort;
+        this.dbService = dbService;
+    }
+
+    //startingpoint depends on what adapter you are using 
+    public List<CoordinateDTO> fetchStartingPointAndEndPoint (String userIDForDB, String destinationID) throws Exception{
+        List<CoordinateDTO> startingPointAndEndPoint = new ArrayList<>();
+        CoordinateDTO startPoint = locationPort.currentCoordinate();
+        CoordinateDTO endPoint = dbService.searchDestination(userIDForDB, destinationID);
+        startingPointAndEndPoint.add(startPoint);
+        startingPointAndEndPoint.add(endPoint);
+
+        return startingPointAndEndPoint;
+    }
+
+}
